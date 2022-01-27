bdc73e9ed78560a8df057e36833608e90006b2a2
Verlanti2002 <79745917+Verlanti2002@users.noreply.github.com>
2022-01-27 21:38:22 +0100

    Delete login_print_variable.php

--- login_print_variable.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $password_crypted = sha1($password);
-
-    echo "Email: " . $email . "<br>";
-    echo "Password: " . $password . "<br>";
-    echo "Password criptata: " . $password_crypted . "<br>"; 
-?>        
